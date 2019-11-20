9dd1fdb40e4a319b597b69b264b4ee8aff999459
Tim Parsons <timmparsons85@gmail.com>
2019-11-19 20:31:47 -0700

    add recursive function

--- ch3/recursion.js
+++ ch3/recursion.js
@@ -1,6 +1,17 @@
+import { isModuleDeclaration } from "@babel/types"
+
 // We’ve seen that % (the remainder operator) can be used to test whether a number is even or odd by using % 2 to see whether it’s divisible by two. Here’s another way to define whether a positive whole number is even or odd:
 // Zero is even.
 // One is odd.
 // For any other number N, its evenness is the same as N - 2.
 // Define a recursive function isEven corresponding to this description. The function should accept a single parameter (a positive, whole number) and return a Boolean.
 // Test it on 50 and 75. See how it behaves on -1. Why? Can you think of a way to fix this?
+
+function isEven(n) {
+  if(n == 0) return true
+  if(n == 0) return false
+  if(n < 0) return isEven(-n)
+  else return isEven(n - 2)
+}
+
+module.exports = isEven;
